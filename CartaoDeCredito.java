0ac0dd205cdb21fae5b03dfcf3b48ee5c1d8dd89
atila9595 <61739459+atila9595@users.noreply.github.com>
2020-03-03 09:32:25 -0300

    alterei
    só para ver

--- CartaoDeCredito.java
+++ CartaoDeCredito.java
@@ -1,18 +1,20 @@
 
+tudo certo
+
 public class CartaoDeCredito {
-	// atributos ou propriedades ou variáveis de instância
+	// atributos ou propriedades ou variÃ¡veis de instÃ¢ncia
 	int numero;
 	double totalFatura;
 
-	// método para retornar todos os dados
+	// mÃ©todo para retornar todos os dados
 	public String retornarDados() {
 		String aux = "";
-		aux += "Número -> " + numero + "\n";
+		aux += "NÃºmero -> " + numero + "\n";
 		aux += "Total da fatura -> R$ " + totalFatura + "\n";
 		return aux;
 	}
 	
-	//método para retornar o total da fatura
+	//mÃ©todo para retornar o total da fatura
 	public double consultarFatura() {
 		return totalFatura;
 	}
